1c1fd3893f64446558da44282170946760326cf9
Virty V. Virtugon <virtugon@users.noreply.github.com>
2024-12-01 14:48:34 +0100

    2024 day 1 part 2

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -11,11 +11,22 @@ for line in lines:
 
 left.sort()
 right.sort()
-    
+
+#  Part 1
 for i in range(len(left)):
     if left[i] > right[i]:
         diff += left[i] - right[i]
     else:
         diff += right[i] - left[i]
 
+# Part 2
+totalScore = []
+for i in left:
+    ctr = 0
+    for j in right:
+        if j == i:
+            ctr += 1
+    totalScore.append(i*ctr)
+
 print(diff)
+print(sum(totalScore))
